ef291bb3e149d96a49202656a23649ce1319fa20
Amine M. Remita <remita.amine@courrier.uqam.ca>
2020-04-07 00:25:47 -0400

    Fix penalty variable in seq_mlr.py

--- scripts/seq_mlr.py
+++ scripts/seq_mlr.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     ###################
 
     #penalty = 'elasticnet'
-    penalty = 'none'
+    penalty = reg_penalty
     alpha = 10 * X_train.shape[0]
     l1_ratio = 0.5
     max_iter = 1000
